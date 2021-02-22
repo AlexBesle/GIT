5eeebde8fc91f3dd4cb0c6af3b6163372990cc7b
AlexBesle <alex_besle@hotmail.com>
2021-02-22 12:07:25 +0100

    modif ajout nav bar

--- index.php
+++ index.php
@@ -10,7 +10,18 @@
 <body>
 <div class="container">
 
+
+<nav class="navbar navbar-light bg-light">
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+</nav>
+
             <h1 class="display-4 text-center my-5">Mon projet Git</h1>
+
+
+            
 
 
 </div>
